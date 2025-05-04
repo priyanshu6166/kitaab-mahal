2a17cdf2e9e1805b23746e067fd4248096caad8b
priyanshu6166 <priyanshukori8171@gmail.com>
2025-05-04 12:08:50 +0530

    Delete books directory

--- books/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('book/<int:book_id>/', views.book_detail, name='book_detail'),
-] 
